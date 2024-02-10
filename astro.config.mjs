5cefc2233bbd7f5f26a5a59109e6700e781c8546
oelopes <oelopes2@gmail.com>
2024-02-10 04:36:26 -0300

    feat: add navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 import vercel from "@astrojs/vercel/serverless";
 
@@ -8,5 +8,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
